58d88b936a3c1c9d8be61cfadff6d76551febb23
SaniJ28 <sanidhyajain28@gmail.com>
2025-05-17 16:05:27 +0530

    Coding Ewercise 2

---
+++ Python/Coding Exercise/Coding Exercise 1.py
@@ -0,0 +1,13 @@
+def celsius_to_fahrenheit(c):
+    """
+    Function to convert temperature from Celsius to Fahrenheit.
+    
+    Parameters:
+    C (float): The temperature in Celsius.
+    
+    Returns:
+    float: The temperature in Fahrenheit.
+    """
+    # Your code here
+    return (c*9/5)+32
+    
